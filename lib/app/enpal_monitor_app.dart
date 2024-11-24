cabbe8738fcfc0f1e120152e19eb5492ecb13d84
Ali Akber <ali.akber@deliveryhero.com>
2024-11-24 14:45:05 +0100

    add: app module

---
+++ lib/app/enpal_monitor_app.dart
@@ -0,0 +1,21 @@
+import 'package:enpal_design_system/styles/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class EnpalMonitorApp extends StatelessWidget {
+  const EnpalMonitorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Current Weather',
+      theme: theme.light(),
+      darkTheme: theme.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World!"),
+        ),
+      ),
+    );
+  }
+}
